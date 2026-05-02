09ae29009971b78d9db8fc79e9fd56334498bded
asanov-aleksandr-vlz <asanov-aleksandr-vlz@yandex.ru>
2026-05-02 15:52:36 +0300

    5.4 Введение в Playwright

---
+++ templates/playwright_registration.py
@@ -0,0 +1,17 @@
+from playwright.sync_api import sync_playwright, expect
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    new_page = browser.new_page()
+    new_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+    email_input = new_page.locator('//div[@data-testid="registration-form-email-input"]//div//input')
+    email_input.fill("user.name@gmail.com")
+    user_name_input = new_page.locator('//div[@data-testid="registration-form-username-input"]//div//input')
+    user_name_input.fill("username")
+    password_input = new_page.locator('//div[@data-testid="registration-form-password-input"]//div//input')
+    password_input.fill("password")
+    registration_button = new_page.locator('//button[@data-testid="registration-page-registration-button"]')
+    registration_button.click()
+    dashboard_alert = new_page.locator('//*[@data-testid="dashboard-toolbar-title-text"]')
+    expect(dashboard_alert).to_be_visible()
+    expect(dashboard_alert).to_have_text("Dashboard")  
+    new_page.wait_for_timeout(5000)
